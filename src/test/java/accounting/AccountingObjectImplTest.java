263b412232e7d3d00ba5d2d7deea62358698f6a1
Aleksei Dolgii <sinc86@gmail.com>
2016-07-03 11:15:26 +0300

    tests

--- src/test/java/accounting/AccountingObjectImplTest.java
+++ src/test/java/accounting/AccountingObjectImplTest.java
@@ -1,5 +1,6 @@
 package accounting;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.stream.IntStream;
@@ -8,6 +9,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class AccountingObjectImplTest {
+
+    private AccountingObject accountingObject;
+
+    @Before
+    public void before() {
+        accountingObject = new AccountingObjectImpl();
+    }
 
     @Test(expected = NullPointerException.class)
     public void testConstructor() throws Exception {
@@ -24,18 +32,17 @@ public class AccountingObjectImplTest {
 
     @Test
     public void testWithManyThreads() throws Exception {
-        AccountingObject obj = new AccountingObjectImpl();
         int threadNumber = 5;
         int eventsPerThread = 1_000;
         IntStream.range(0, threadNumber).mapToObj(i -> new Thread(() ->
-                IntStream.range(0, eventsPerThread).forEach(k -> obj.registerEvent())
+                IntStream.range(0, eventsPerThread).forEach(k -> accountingObject.registerEvent())
         )).map(t -> {
             t.start();
             return t;
         }).forEach(this::joinThread);
-        assertEquals(obj.lastMinuteCount(), threadNumber * eventsPerThread);
-        assertEquals(obj.lastHourCount(), threadNumber * eventsPerThread);
-        assertEquals(obj.lastDayCount(), threadNumber * eventsPerThread);
+        assertEquals(accountingObject.lastMinuteCount(), threadNumber * eventsPerThread);
+        assertEquals(accountingObject.lastHourCount(), threadNumber * eventsPerThread);
+        assertEquals(accountingObject.lastDayCount(), threadNumber * eventsPerThread);
     }
 
     @Test
